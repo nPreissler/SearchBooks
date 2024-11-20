a8f6a2a89a841f0807aa4a13bc78206e12d6d637
Nestor <148357514+nPreissler@users.noreply.github.com>
2024-11-20 18:51:17 -0300

    feat: creating a html page for user interctions with API book data

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import requests
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
 
-search = input('Search for a book: ')
+@app.route('/')
 
-api_key = 'API_KEY'
-query = search
-url = f'https://www.googleapis.com/books/v1/volumes?q={query}&key={api_key}'
+def home():
+    key = 'API_KEY'
+    query = request.args.get('query', 'awordforshownoresultsinthepage')
+    url = f'https://www.googleapis.com/books/v1/volumes?q={query}&key={key}'
+    response = requests.get(url)
 
-response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        books = [
+            {
+                'title': book['volumeInfo'].get('title', 'Título não disponível'),
+                'authors': book['volumeInfo'].get('authors', ['Autor não disponível']),
+                'description': book['volumeInfo'].get('description', 'Descrição não disponível'),
+            }
+            for book in data.get('items', [])
+        ]
+    else:
+        books = []
 
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f'Erro: {response.status_code}')
+    return render_template('index.html', books=books)
 
+if __name__ == '__main__':
+    app.run(debug=True)
